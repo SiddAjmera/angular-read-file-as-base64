8a3c37284f8101714a2bc41446fd45dd30d4f033
SiddAjmera <siddharthajmeracs@hotmail.com>
2019-06-20 23:24:41 +1000

    2 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('/assets/image.jpg', { responseType: 'blob' })
+      .subscribe(res => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          var base64data = reader.result;                
+              console.log(base64data);
+        }
+
+        reader.readAsDataURL(res); 
+        console.log(res);
+      });
+  }
 }
